284e5da165dd99d03c0eb748f851cb03cedb5a9a
yaolubrain <yaolubrain@gmail.com>
2015-10-07 12:45:21 +0300

    n

---
+++ imagenet_zero_shot_unseen_wnids.py
@@ -0,0 +1,9 @@
+imagenet_21k = open('imagenet.synset.obtain_synset_list', 'r').read().splitlines()
+
+imagenet_1k = open('distance_mat_wordnet_nodes_1K.txt', 'r').read().splitlines()
+idx_list = open('imagenet_zero_shot_unseen_wnids.txt', 'w')
+
+for line in imagenet_21k:
+    if not line in imagenet_1k:
+        idx_list.write(line+'\n')
+
