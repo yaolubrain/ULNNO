c0177ea82e741c55fc71ef9b08253e58b5180e69
yaolubrain <yaolubrain@gmail.com>
2015-10-15 16:05:07 +0300

    cleaning code

--- imagenet_zero_shot_unseen_wnids.py
+++ imagenet_zero_shot_unseen_wnids.py
@@ -1,5 +1,4 @@
 imagenet_21k = open('imagenet.synset.obtain_synset_list', 'r').read().splitlines()
-
 imagenet_1k = open('distance_mat_wordnet_nodes_1K.txt', 'r').read().splitlines()
 idx_list = open('imagenet_zero_shot_unseen_wnids.txt', 'w')
 
